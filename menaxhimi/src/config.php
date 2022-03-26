c956ccfd49cb38e16f4a20da887f91412ddb6ada
OLDI-PC\Feriold <ferjolt@ozuni.net>
2022-03-26 03:06:50 +0100

    Caktivizohet debug per sistemin e menaxhimit

--- menaxhimi/src/config.php
+++ menaxhimi/src/config.php
@@ -126,7 +126,7 @@ require_once __DIR__ . "/userlevelsettings.php";
 $CONFIG = [
 
     // Debug
-    "DEBUG" => true, // Enabled
+    "DEBUG" => false, // Enabled
     "REPORT_ALL_ERRORS" => false, // Treat PHP warnings and notices as errors
     "LOG_ERROR_TO_FILE" => false, // Log error to file
     "DEBUG_MESSAGE_TEMPLATE" => '<div class="card card-danger ew-debug"><div class="card-header">' .
@@ -145,7 +145,7 @@ $CONFIG = [
 
     // General
     "UNFORMAT_YEAR" => 50, // Unformat year
-    "RANDOM_KEY" => '30XpEU49X1NY9tqd', // Random key for encryption
+    "RANDOM_KEY" => 'nf8CEvGayaQs5QgE', // Random key for encryption
     "ENCRYPTION_KEY" => '', // Encryption key for data protection
     "PROJECT_STYLESHEET_FILENAME" => "css/volalservice.css", // Project stylesheet file name
     "USE_COMPRESSED_STYLESHEET" => true, // Compressed stylesheet
