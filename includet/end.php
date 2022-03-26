61550ed89b9dc9e12d31ec394efeb3fc770aa4fd
OLDI-PC\Feriold <ferjolt@ozuni.net>
2022-03-26 03:18:40 +0100

    Rregullohen te dhenat e serverit smtp per sistemin e menaxhimit

--- includet/end.php
+++ includet/end.php
@@ -1,11 +1,11 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-C9KY99E4J4"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-3ZD3PE3S1E"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-C9KY99E4J4');
+  gtag('config', 'G-3ZD3PE3S1E');
 </script>
 
     </body>
